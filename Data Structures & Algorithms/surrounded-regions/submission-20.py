efa9d2cd23e77da8fcbf42b90efe99c8212a7df2
EricW2003 <erik2865385544@gmail.com>
2026-09-01 22:58:16 +0200

    Add: surrounded-regions - submission-20

---
+++ Data Structures & Algorithms/surrounded-regions/submission-20.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        visited = set()
+        n, m = len(board), len(board[0])
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j]=="O" and (i,j) not in visited:
+                    not_surr = False
+                    stack = [(i,j)]
+                    explored = []
+                    while stack:
+                        node = stack.pop()
+                        if node in visited:
+                            continue
+                        visited.add(node)
+                        
+                        a, b = node
+                        explored.append((a,b))
+                        
+                        not_surr
+                        if a==0 or a==n-1 or b==0 or b==m-1:
+                            not_surr = True
+                        
+                        
+                        for dx,dy in directions:
+                            x, y = a+dx, b+dy
+                            if 0<=x<n and 0<=y<m and board[x][y]=="O":
+                                stack.append((x,y))
+                    if not not_surr:
+                        for k,l in explored:
+                            board[k][l]="X"
+        return
