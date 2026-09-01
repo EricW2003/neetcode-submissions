b37c2cea4410477f81ea9258aa15349dd7c90ecd
EricW2003 <erik2865385544@gmail.com>
2026-09-01 16:26:48 +0200

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        n,m = len(grid), len(grid[0])
+        starts = []
+        # We need to find all the treseaures first and start from them
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]==0:
+                    starts.append((i,j))
+
+        q = deque(starts)
+        visited = set(starts)
+        while q:
+            node = q.popleft()
+            directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+            for dx,dy in directions:
+                i,j = node
+                x,y = i+dx,j+dy
+                if 0<=x<n and 0<=y<m:
+                    if (x,y) not in visited and grid[x][y]!=-1:
+                        visited.add((x,y))
+                        grid[x][y]= min(grid[x][y],1+grid[i][j])
+                        q.append((x,y))
+
+        return 
