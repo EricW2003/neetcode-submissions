9d258261213b6e043f261f01450bed15abf8f301
EricW2003 <erik2865385544@gmail.com>
2026-09-05 18:56:28 +0200

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        d = [0]*26
+        for i,letter in enumerate(s):
+            d[ord(letter)-ord("a")]=i
+        ans = []
+        min_end = 0 
+        start = 0
+        for t, letter in enumerate(s):
+            min_end = max(min_end, d[ord(letter)-ord("a")])
+            
+            if min_end == t:
+                ans.append(min_end-start+1)
+                start = t+1
+
+        return ans
